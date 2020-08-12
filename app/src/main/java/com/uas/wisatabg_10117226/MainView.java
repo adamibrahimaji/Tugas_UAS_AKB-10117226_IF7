29f4c1c41055608d7f880037268245882433be63
Adam Ibrahim <adaminteristi2000@gmail.com>
2020-08-12 14:33:23 +0700

    Well Done

---
+++ app/src/main/java/com/uas/wisatabg_10117226/MainView.java
@@ -0,0 +1,10 @@
+package com.uas.wisatabg_10117226;
+/*
+ * Nama : Mufti Alfarisy
+ * Kelas : IF 7
+ * NIM : 10117229
+ * tgl pengerjaan : 7 agustus - 12 agustus 2020
+ * */
+public interface MainView {
+    void tampilkandata(User user);
+}
